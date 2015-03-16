1df4bc05a7e42ad0a6b1c576205f3e1417b72ca5
arvinls <liuxing_8810@126.com>
2015-03-17 07:25:36 +0800

    add the flask-script into this program

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
-from flask import Flask 
-app = Flask(__name__)
+from flask import Flask
+from flask.ext.script import Manager
 
+app = Flask(__name__)
+manager = Manager(app) 
 @app.route('/')
 def index():
 	return '<h1>Hello World!</h1>'
@@ -10,4 +12,4 @@ def user(name):
 	return '<h1>Hello, %s</h1>' % name
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	manager.run()
