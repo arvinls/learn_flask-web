53ddfb7d65a871973c05b67ade79da889bbdd6d1
arvinls <liuxing_8810@126.com>
2015-03-20 12:37:40 +0800

    modified a small bug

--- hello_5b.py
+++ hello_5b.py
@@ -70,7 +70,8 @@ def index():
 		form.name.data = ''
 		return redirect(url_for('index'))
 	return render_template('index.html',
-		form = form, name = session.get('name'), current_time = datetime.utcnow())
+		form = form, name = session.get('name'), known = session.get('known', False),
+		current_time = datetime.utcnow())
 
 
 
