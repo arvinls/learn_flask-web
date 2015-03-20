56bcce178db63a45144eac77ec423147a67df3f3
arvinls <liuxing_8810@126.com>
2015-03-20 13:26:58 +0800

    this part is about the flask-migrate, but there are some questions which has not been solved.

---
+++ venv/Scripts/alembic-script.py
@@ -0,0 +1,10 @@
+#!D:\flask\arvin\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'alembic==0.7.5.post1','console_scripts','alembic'
+__requires__ = 'alembic==0.7.5.post1'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('alembic==0.7.5.post1', 'console_scripts', 'alembic')()
+    )
